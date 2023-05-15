669f86e88d614d4ce4a682d5392eac2ec15ba709
unknown <jensennics.dlrs@gmail.com>
2023-05-16 01:55:49 +0800

    Code Finalized.

--- Lion.java
+++ Lion.java
@@ -3,13 +3,13 @@ public class Lion extends Mammal {
         super(name, age, gender);
     }
 
-    // //Override Speak Method
-    // public void speak() {
-    //     System.out.println("Roar! Roar!");
-    // }
+    //Override Speak Method
+    public void speak() {
+        System.out.println("Roar! Roar!");
+    }
 
-    // //Override Display Method
-    // public void displayInfo() {
-    //     System.out.println("Lion - Name: " + getName() + ", Age: " + getAge() + ", Gender: " + getGender());
-    //}
+    //Override Display Method
+    public void displayInfo() {
+        System.out.println("Lion - Name: " + getName() + ", Age: " + getAge() + ", Gender: " + getGender());
+    }
 }
