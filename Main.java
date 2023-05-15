9010022ebc5a75d253905e1c1509c5be2bf96aec
unknown <jensennics.dlrs@gmail.com>
2023-05-16 01:38:44 +0800

    Grouped all the instantiation to make the code neat.

--- Main.java
+++ Main.java
@@ -1,33 +1,40 @@
+// Create a main class, do the following:
+// a. Instantiate different animal objects.
+// b. Call the displayInfo() and speak() methods to display the animals' information and 
+// speak value.
+
 public class Main {
     public static void main(String[] args) {
-        
+
         System.out.print("\n"); //Empty Line
+        
         Lion lion = new Lion("Mufasa", 5, "Male");
+        Elephant elephant = new Elephant("Adira", 13, "Female");
+        Snake snake = new Snake("Nagini", 9, "Male");
+        Lizard lizard = new Lizard("Raplo", 4, "Male");
+        Eagle eagle = new Eagle("Artemis", 21, "Female");
+        Parrot parrot = new Parrot("Polly", 4, "Female");
+
         lion.displayInfo();
         lion.speak();
         System.out.print("\n");
 
-        Elephant elephant = new Elephant("Adira", 13, "Female");
         elephant.displayInfo();
         elephant.speak();
         System.out.print("\n");
 
-        Snake snake = new Snake("Nagini", 9, "Male");
         snake.displayInfo();
         snake.speak();
         System.out.print("\n");
 
-        Lizard lizard = new Lizard("Raplo", 4, "Male");
         lizard.displayInfo();
         lizard.speak();
         System.out.print("\n");
 
-        Eagle eagle = new Eagle("Artemis", 21, "Female");
         eagle.displayInfo();
         eagle.speak();
         System.out.print("\n");
 
-        Parrot parrot = new Parrot("Polly", 4, "Female");
         parrot.displayInfo();
         parrot.speak();
         System.out.print("\n");
